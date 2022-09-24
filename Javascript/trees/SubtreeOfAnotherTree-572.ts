7fecf329332a7a07e08f16dbc6f7b78e69fd7210
sathyajitloganathan <loganathansathyajit@gmail.com>
2022-09-24 13:01:19 +0800

    Add answer for SubtreeOfAnotherTree

---
+++ Javascript/trees/SubtreeOfAnotherTree-572.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    function traverse(r: TreeNode | null) {
+        if (r == null) {
+            return false;
+        } else if (subRoot == null) {
+            return true;
+        }
+
+        if (compareTrees(r, subRoot)) {
+            return true;
+        } else {
+            return traverse(r.left) || traverse(r.right);
+        }
+
+        // return false;
+    }
+
+    function compareTrees(p: TreeNode | null, q: TreeNode | null) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p && q && p.val == q.val) {
+            return compareTrees(p.left, q.left) && compareTrees(p.right, q.right);
+        }
+        return false;
+    }
+
+    return traverse(root);
+
+};
